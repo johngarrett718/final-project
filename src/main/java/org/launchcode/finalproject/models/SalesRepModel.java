3361e6406d1652ce528b187db30c2cd484913437
John Garrett <john.garrett718@gmail.com>
2017-08-13 11:43:59 -0500

    Added Fields to SalesRepModel

---
+++ src/main/java/org/launchcode/finalproject/models/SalesRepModel.java
@@ -0,0 +1,24 @@
+package org.launchcode.finalproject.models;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+public class SalesRepModel {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 2, max = 15)
+    private String firstName;
+
+    @NotNull
+    @Size(min = 2, max = 15)
+    private String lastName;
+
+}
