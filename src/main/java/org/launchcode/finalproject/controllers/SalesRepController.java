65a474796987e471770062a871e91574778ab7b9
John Garrett <john.garrett718@gmail.com>
2017-09-03 16:38:03 -0500

    Worked on SalesRepController & add.html

--- src/main/java/org/launchcode/finalproject/controllers/SalesRepController.java
+++ src/main/java/org/launchcode/finalproject/controllers/SalesRepController.java
@@ -1,11 +1,45 @@
 package org.launchcode.finalproject.controllers;
 
+import org.launchcode.finalproject.models.SalesRepModel;
+import org.launchcode.finalproject.models.data.SalesRepDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 
 @Controller
-@RequestMapping("salesRep")
+@RequestMapping("salesrep")
 public class SalesRepController {
+    @Autowired
+    private SalesRepDao salesrepDao;
 
+    @RequestMapping(value = "")
+    public String index(Model model) {
+//        model.addAttribute("salesrep", salesrepDao.findAll());
+        model.addAttribute("title", "Sales Reps");
+
+        return "salesrep/index";
+
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid
+            SalesRepModel salesRep, Errors error) {
+        if (error.hasErrors()) {
+            model.addAttribute("title", "Add a Sales Rep");
+            return "salesrep/add";
+        }
+        salesrepDao.save(salesRep);
+        return "redirect:";
+    }
 }
+
+
+
+
