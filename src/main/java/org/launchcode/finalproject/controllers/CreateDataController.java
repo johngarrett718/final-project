a36b48e7747a49c423c8f1e4839f24fd10b4a2a4
John Garrett <john.garrett718@gmail.com>
2018-01-23 19:50:07 -0600

    Added a for loop to loop over every dy of a given year

--- src/main/java/org/launchcode/finalproject/controllers/CreateDataController.java
+++ src/main/java/org/launchcode/finalproject/controllers/CreateDataController.java
@@ -3,6 +3,8 @@ package org.launchcode.finalproject.controllers;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.launchcode.finalproject.models.ClientModel;
+import org.launchcode.finalproject.models.SaleModel;
+import org.launchcode.finalproject.models.SalesRepModel;
 import org.launchcode.finalproject.models.data.ClientDao;
 import org.launchcode.finalproject.models.data.SaleDao;
 import org.launchcode.finalproject.models.data.SalesRepDao;
@@ -15,6 +17,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 @Controller
@@ -55,6 +61,30 @@ public class CreateDataController {
 
             clientDao.save(newClient);
         }
+        in = new FileReader("Users/johng/Desktop/SalesRep.csv");
+        records = CSVFormat.EXCEL.withHeader().parse(in);
+        for (CSVRecord salesrep : records){
+            SalesRepModel newSalesRep = new SalesRepModel();
+            newSalesRep.setFirstName(salesrep.get("FirstName"));
+            newSalesRep.setLastName(salesrep.get("LastName"));
+
+            salesRepDao.save(newSalesRep);
+        }
+        SimpleDateFormat formatter=new SimpleDateFormat("MM-dd-yyyy");
+        GregorianCalendar cal=new GregorianCalendar();
+        int year=2013;
+        int total=365;
+        cal.set(Calendar.YEAR, year);
+        if (cal.isLeapYear(year)) {
+            total++;
+        }
+
+        for(int d=1; d<=total; d++) {
+            cal.set(Calendar.DAY_OF_YEAR, d);
+            Date date = cal.getTime();
+        }
+
         return "redirect:";
     }
 }
+
